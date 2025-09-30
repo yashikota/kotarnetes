04c2f8b062c718891894bab4a75d719ee9a8f9b0
yashikota <52403688+yashikota@users.noreply.github.com>
2025-09-30 23:06:55 +0900

    fix: filepath

--- scripts/vm.sh
+++ scripts/vm.sh
@@ -3,12 +3,12 @@ sudo apt install -y incus qemu-system
 sudo adduser $USER incus-admin
 
 # Initialize Incus
-sudo incus admin init --preseed < incus-init.yaml
+sudo incus admin init --preseed < scripts/incus-init.yaml
 
 # Create VM with cloud-init
-sudo incus launch images:ubuntu/24.04/cloud k8s-master --vm --config=user.user-data="$(cat cloud-init.yaml)"
-sudo incus launch images:ubuntu/24.04/cloud k8s-worker1 --vm --config=user.user-data="$(cat cloud-init.yaml)"
-sudo incus launch images:ubuntu/24.04/cloud k8s-worker2 --vm --config=user.user-data="$(cat cloud-init.yaml)"
+sudo incus launch images:ubuntu/24.04/cloud k8s-master --vm --config=user.user-data="$(cat scripts/cloud-init.yaml)"
+sudo incus launch images:ubuntu/24.04/cloud k8s-worker1 --vm --config=user.user-data="$(cat scripts/cloud-init.yaml)"
+sudo incus launch images:ubuntu/24.04/cloud k8s-worker2 --vm --config=user.user-data="$(cat scripts/cloud-init.yaml)"
 
 # Check VM status
 sudo incus list
